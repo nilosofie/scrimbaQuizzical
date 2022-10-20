8229424528cf7326a402811eeee7fc93416d1049
Nicolaas Thiart <nicolaasthiart@pop-os.localdomain>
2022-10-20 05:48:15 +0200

    Basic Shell

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import Question from './Question';
 import { nanoid } from 'nanoid';
-import quizData from '../data';
+// import quizData from '../data';
+
+//Component==============================================================================================
 
 export default function Main(props) {
+  //States----------------------------------------------------------------------------------------
   const [quizState, setQuizState] = React.useState();
+  const [markState, setMarkState] = React.useState(false);
+  const [quizElements, setQuizElements] = React.useState();
+
+  //UseEffects----------------------------------------------------------------------------------------------
+  React.useEffect(() => {
+    getApiQuiz();
+    // console.log(quizData);
+    // setQuizState(quizData);
+  }, []);
+
+  React.useEffect(() => {
+    setQuizElements(
+      quizState ? (
+        quizState.results.map((questionSet) => (
+          <Question
+            question={questionSet.question}
+            correct={questionSet.correct_answer}
+            incorrect={questionSet.incorrect_answers}
+            key={nanoid()}
+          />
+        ))
+      ) : (
+        <h3>Loading...</h3>
+      )
+    );
+  }, [quizState]);
+
+  //Functions-------------------------------------------------------------------------------------------------
 
   const getApiQuiz = async () => {
     const response = await fetch('https://opentdb.com/api.php?amount=5').then(
@@ -14,31 +45,34 @@ export default function Main(props) {
     setQuizState(response);
   };
 
-  React.useEffect(() => {
-    getApiQuiz();
-    // console.log(quizData);
-    // setQuizState(quizData);
-  }, []);
+  // Handlers----------------------------------------------------------------------------------------------
+  function handleMarkState() {
+    setMarkState((old) => !old);
+  }
 
-  const questionElements = quizState ? (
-    quizState.results.map((questionSet) => (
-      <Question
-        question={questionSet.question}
-        correct={questionSet.correct_answer}
-        incorrect={questionSet.incorrect_answers}
-        key={nanoid()}
-      />
-    ))
-  ) : (
-    <h3>Loading...</h3>
-  );
+  //Elements------------------------------------------------------------------------------------------------
+
+  // const questionElements = quizState ? (
+  //   quizState.results.map((questionSet) => (
+  //     <Question
+  //       question={questionSet.question}
+  //       correct={questionSet.correct_answer}
+  //       incorrect={questionSet.incorrect_answers}
+  //       key={nanoid()}
+  //     />
+  //   ))
+  // ) : (
+  //   <h3>Loading...</h3>
+  // );
+
+  //Render-----------------------------------------------------------------------------------------------
 
   return (
     <div className="main-div">
       {/* <pre>{JSON.stringify(quizState.results, null, 2)}</pre> */}
-      <div>{questionElements}</div>
-
-      <button onClick={props.handleIntro} className="btn">
+      <div>{quizElements}</div>
+      <p>markState {markState ? 'True' : 'False'}</p>
+      <button onClick={handleMarkState} className="btn">
         Check Awnsers
       </button>
     </div>
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,19 +1,37 @@
 import React from 'react';
+import { nanoid } from 'nanoid';
 // import Answer from './Answer';
 
+//Component=============================================================================
+
 function Answer(props) {
-  return <div className="answer-div unselected">{props.value}</div>;
+  const styles = props.selected
+    ? 'answer-div selected'
+    : 'answer-div unselected';
+
+  return (
+    <div className={styles} onClick={props.selectAns}>
+      {props.value}
+    </div>
+  );
 }
 
+//Component==============================================================================
+
 export default function Question(props) {
+  //Sate------------------------------------------------------------
   const [arrAns, setArrAns] = React.useState(scramble);
+
+  //UseEffect-----------------------------------------------------------
+
+  //Functions------------------------------------------------------------
 
   function shuffle(array) {
     let currentIndex = array.length,
       randomIndex;
 
     // While there remain elements to shuffle.
-    while (currentIndex != 0) {
+    while (currentIndex !== 0) {
       // Pick a remaining element.
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
@@ -24,36 +42,56 @@ export default function Question(props) {
         array[currentIndex],
       ];
     }
-
     return array;
   }
+
+  //-----------------------------------------------------------------------------
 
   function scramble() {
     const arrScram = [
       {
-        id: 0,
         value: props.correct,
         mark: true,
         selected: false,
+        id: nanoid(),
       },
     ];
 
     for (let i = 0; i < props.incorrect.length; i++) {
       arrScram.push({
-        id: i,
         value: props.incorrect[i],
         mark: false,
         selected: false,
+        id: nanoid(),
       });
     }
-
     return shuffle(arrScram);
   }
 
-  const answerElements = arrAns.map((answer) => (
-    <Answer value={answer.value} mark={answer.mark} />
-  ));
+  //Handler----------------------------------------------------------------------------------
 
+  function handleSelect(id) {
+    setArrAns((oldArr) =>
+      oldArr.map((ans) => {
+        return ans.id === id
+          ? { ...ans, selected: true }
+          : { ...ans, selected: false };
+      })
+    );
+  }
+
+  //Elements----------------------------------------------------------------------------------
+
+  const answerElements = arrAns.map((answer) => (
+    <Answer
+      value={answer.value}
+      mark={answer.mark}
+      selected={answer.selected}
+      key={answer.id}
+      selectAns={() => handleSelect(answer.id)}
+    />
+  ));
+  //Render--------------------------------------------------------------------------
   return (
     <div className="question-div">
       <h3>{props.question}</h3>
